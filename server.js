470a55b8a27eaac302e75da23805f325f38a807b
TheOneNite <travisrilea@gmail.com>
2021-06-19 00:15:51 -0400

    port change

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const express = require("express");
-const app = express();
-const fs = require("fs");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const listenPort = 4000;
-const manifestPath = __dirname + "/manifest.json";
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const listenPort = 3389
+const manifestPath = __dirname + '/manifest.json'
 
 const rootDir =
-  "C:/Users/Travis/Documents/Workspaces/MiniMars/Mission2Mars_depot/Distribution/builds/dev/m2m";
+  'C:/Users/Travis/Documents/Workspaces/MiniMars/Mission2Mars_depot/Distribution/builds/dev/m2m'
 
-app.use(bodyParser.json());
-app.use(cors({ origin: true }));
+app.use(bodyParser.json())
+app.use(cors({ origin: true }))
 
-app.get("/manifest", (req, res) => {
-  console.log("GET/manifest");
-  fs.readFile(manifestPath, "utf-8", (e, data) => {
+app.get('/manifest', (req, res) => {
+  console.log('GET/manifest')
+  fs.readFile(manifestPath, 'utf-8', (e, data) => {
     if (e) {
-      console.log(e);
+      console.log(e)
     }
-    console.log(data);
-    res.send(JSON.stringify(data));
-  });
-});
+    console.log(data)
+    res.send(JSON.stringify(data))
+  })
+})
 
-app.get("/install-full", (req, res) => {});
-app.post("/fetch-file", (req, res) => {
-  console.log(`POST/fetch-file`);
-  console.log(req.body);
-  let path = req.body.file;
-  path = path.replace(":root", rootDir);
-  res.setHeader("content-type", "arraybugger");
-  fs.createReadStream(path).pipe(res);
-});
+app.get('/install-full', (req, res) => {})
+app.post('/fetch-file', (req, res) => {
+  console.log(`POST/fetch-file`)
+  console.log(req.body)
+  let path = req.body.file
+  path = path.replace(':root', rootDir)
+  res.setHeader('content-type', 'arraybugger')
+  fs.createReadStream(path).pipe(res)
+})
 
 app.listen(listenPort, () => {
-  console.log(`fileServer up on ${listenPort}`);
-});
+  console.log(`fileServer up on ${listenPort}`)
+})
