528f84d0d531458afd73468e781e7bb40c8f0ff1
TheOneNite <travisrilea@gmail.com>
2021-06-27 21:57:57 -0400

    make manifest path relative, remove game builds from repo

--- generateManifest.js
+++ generateManifest.js
@@ -1,48 +1,47 @@
-const crypto = require("crypto");
-const fs = require("fs");
-const manifest = require("./utils/manifest");
+const crypto = require('crypto')
+const fs = require('fs')
+const manifest = require('./utils/manifest')
 
 //this is fake for dev
-const rootDir =
-  "C:/Users/Travis/Documents/Workspaces/MiniMars/Mission2Mars_depot/Distribution/builds/dev/m2m";
+const rootDir = `${__dirname}/builds/dev/m2m`
 
 const generateManifest = async (rootDir) => {
-  const files = manifest.generate(rootDir);
+  const files = manifest.generate(rootDir)
   let hashes = await Promise.all(
     files.map(async (path) => {
       const hash = new Promise((resolve, reject) => {
-        console.log("hash", path);
-        let hash = crypto.createHash("md5");
-        let stream = fs.createReadStream(path);
-        stream.on("error", (e) => {
-          throw e;
-          reject(e);
-        });
-        stream.on("data", (data) => {
-          hash.update(data);
-        });
-        stream.on("close", () => {
-          resolve(hash.digest("hex"));
-        });
-      });
-      return hash;
+        console.log('hash', path)
+        let hash = crypto.createHash('md5')
+        let stream = fs.createReadStream(path)
+        stream.on('error', (e) => {
+          throw e
+          reject(e)
+        })
+        stream.on('data', (data) => {
+          hash.update(data)
+        })
+        stream.on('close', () => {
+          resolve(hash.digest('hex'))
+        })
+      })
+      return hash
     })
-  );
+  )
   let checksums = files.map((path, i) => {
     return {
-      path: path.replace(rootDir, ":root"),
+      path: path.replace(rootDir, ':root'),
       hash: hashes[i],
-    };
-  });
-  const checksumJSON = JSON.stringify(checksums, undefined, "\t");
+    }
+  })
+  const checksumJSON = JSON.stringify(checksums, undefined, '\t')
   fs.writeFile(`${__dirname}/manifest.json`, checksumJSON, (e) => {
     if (e) {
-      throw e;
+      throw e
     }
-  });
-  console.log(checksums);
-};
+  })
+  console.log(checksums)
+}
 
-generateManifest(rootDir);
+generateManifest(rootDir)
 
-module.exports = { generateManifest };
+module.exports = { generateManifest }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const listenPort = 3389
 const manifestPath = __dirname + '/manifest.json'
+// to upload: scp -r ./MissionToMars.zip root@missiontomars.ca:~/m2m-production/build/static/media
 
 const rootDir = __dirname + '/builds/dev/m2m'
 
@@ -28,7 +29,7 @@ app.post('/fetch-file', (req, res) => {
   console.log(req.body)
   let path = req.body.file
   path = path.replace(':root', rootDir)
-  res.setHeader('content-type', 'arraybugger')
+  res.setHeader('content-type', 'stream')
   fs.createReadStream(path).pipe(res)
 })
 
